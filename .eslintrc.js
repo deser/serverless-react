f90e8bf6d7f87a65a49ca41f2437c1657b567a1b
aliaksei_petrachenka <aliaksei_petrachenkau@epam.com>
2021-10-26 23:07:59 +0300

    fix: sls deployment feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    "jest/globals": true,
+  },
+  extends: ["plugin:react/recommended", "standard", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "jest", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+    "no-use-before-define": 0,
+  },
+};
